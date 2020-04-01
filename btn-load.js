4138834ffe981fe16e597c17e9c8fcf6a3ae183d
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-04-01 17:11:54 +0200

    Initial commit

---
+++ btn-load.js
@@ -0,0 +1,11 @@
+var BtnLoad = Object.getPrototypeOf(app).BtnLoad = new Component("btn-load");
+BtnLoad.debug = true;
+BtnLoad.createdAt      = "2.0.0";
+BtnLoad.lastUpdate     = "2.0.0";
+BtnLoad.version        = "1";
+// BtnLoad.factoryExclude = true;
+// BtnLoad.loadingMsg     = "This message will display in the console when component will be loaded.";
+
+// BtnLoad.prototype.onCreate = function(){
+  // do thing after element's creation
+// }
