ad1393f4a237676a62039e0994236084a5a44239
Quentin Vansteene <vansteene.quentin@gmail.com>
2020-04-22 17:38:20 +0200

    update module exportation

--- btn-load.js
+++ btn-load.js
@@ -1,5 +1,5 @@
-var BtnLoad = module.exports = function(app){
-  BtnLoad = Object.getPrototypeOf(app).BtnLoad = new app.Component("btn-load");
+module.exports = function(app){
+  var BtnLoad = Object.getPrototypeOf(app).BtnLoad = new app.Component("btn-load");
   // BtnLoad.debug = true;
   BtnLoad.createdAt      = "2.0.0";
   BtnLoad.lastUpdate     = "2.0.0";
